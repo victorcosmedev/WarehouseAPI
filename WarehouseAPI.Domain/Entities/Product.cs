f9a084d053cb0379ad005c4e1de6a0565727dfd1
victorcosmedev <victorcosme.profissional@gmail.com>
2025-12-30 18:02:55 -0300

    feat: adding public and private constructor to Product

--- WarehouseAPI.Domain/Entities/Product.cs
+++ WarehouseAPI.Domain/Entities/Product.cs
@@ -8,10 +8,20 @@ namespace WarehouseAPI.Domain.Entities
 {
     public class Product : CoreEntity
     {
+        private Product() { }
+        public Product(string name, string description, int stock, decimal price)
+        {
+            Name = name;
+            Description = description;
+            Stock = stock;
+            Price = price;
+        }
+        
+
         public string Name { get; private set; }
         public string Description { get; private set; }
         public int Stock { get; private set; }
-        public double Price { get; private set; }
+        public decimal Price { get; private set; }
         public bool Active { get; private set; }
     }
 }
