41df79fcab6fb559511421db622d22c0226b222d
victorcosmedev <victorcosme.profissional@gmail.com>
2025-12-30 19:07:05 -0300

    feat: creating DomainException and applying to Product constructor.

--- WarehouseAPI.Domain/Entities/Product.cs
+++ WarehouseAPI.Domain/Entities/Product.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WarehouseAPI.Domain.Exceptions;
 
 namespace WarehouseAPI.Domain.Entities
 {
     public class Product : CoreEntity
     {
         private Product() { }
+
+        /// <summary>
+        /// Creates a new Product.
+        /// </summary>
+        /// <exception cref="DomainException">Thrown when name is empty or price is negative.</exception>
         public Product(string name, string description, int stock, decimal price)
         {
+            EnsureValidState();
+
             Name = name;
             Description = description;
             Stock = stock;
@@ -24,5 +32,17 @@ namespace WarehouseAPI.Domain.Entities
         public int Stock { get; private set; }
         public decimal Price { get; private set; }
         public bool Active { get; private set; }
+
+
+        private void EnsureValidState()
+        {
+            DomainException.When(string.IsNullOrEmpty(Name), $"{nameof(Name)} cannot be empty.");
+            DomainException.When(string.IsNullOrEmpty(Description), $"{nameof(Description)} cannot be empty.");
+
+            DomainException.When(Stock < 0, $"{nameof(Stock)} cannot be less than zero.");
+            DomainException.When(Price < 0, $"{nameof(Price)} cannot be less than zero.");
+
+            DomainException.When(Active == false, $"{nameof(Active)} cannot be false on creation");
+        }
     }
 }
---
+++ WarehouseAPI.Domain/Exceptions/DomainException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseAPI.Domain.Exceptions;
+
+public class DomainException : Exception
+{
+    public DomainException(string error) : base(error)
+    {
+    }
+
+    public static void When(bool condition, string error)
+    {
+        if (condition)
+        {
+            throw new DomainException(error);
+        }
+    }
+}
