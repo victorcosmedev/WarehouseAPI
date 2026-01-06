cd72397ab8a2a7d5f950228d51b37390747abdb5
victorcosmedev <victorcosme.profissional@gmail.com>
2026-01-06 20:01:42 -0300

    chore: creating IoC project and bootstrap dependency injection file

---
+++ WarehouseAPI.IoC/Bootstrap.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WarehouseAPI.Infra.Data.AppData;
+
+namespace WarehouseAPI.IoC;
+
+public class Bootstrap
+{
+    public static void AddIoC(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddSingleton<DbContext, ApplicationContext>();
+    }
+}
