0c40cdd4f96feba9ce962170a40247475d0a12f2
victorcosmedev <victorcosme.profissional@gmail.com>
2026-01-07 19:08:17 -0300

    feat: implementing Unit of Work pattern

---
+++ WarehouseAPI.Infra.Data/AppData/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WarehouseAPI.Domain.Interfaces;
+
+namespace WarehouseAPI.Infra.Data.AppData
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationContext _context;
+        public UnitOfWork(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> CommitAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
