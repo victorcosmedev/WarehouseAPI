34948c373e1439574e7968083c988728b24bd44f
victorcosmedev <victorcosme.profissional@gmail.com>
2026-01-17 20:07:13 -0300

    feat (presentation): adding ProductsController.

---
+++ WarehouseAPI/Controllers/v1/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using WarehouseAPI.Application.Application.Products.Commands;
+
+namespace WarehouseAPI.Presentation.Controllers.v1
+{
+    [ApiController]
+    [Route("/api/v{version:apiVersion}/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ProductsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateProductCommand command)
+        {
+            var response = await _mediator.Send(command);
+
+            return CreatedAtAction(nameof(Create), response);
+        }
+    }
+}
