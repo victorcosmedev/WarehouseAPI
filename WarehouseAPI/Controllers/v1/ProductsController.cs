df60f0271a07288f71d257f95e707c5b6b3f0535
victorcosmedev <victorcosme.profissional@gmail.com>
2026-02-25 20:35:16 -0300

    chore: creating endpoint for query

--- WarehouseAPI/Controllers/v1/ProductsController.cs
+++ WarehouseAPI/Controllers/v1/ProductsController.cs
@@ -1,6 +1,7 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using WarehouseAPI.Application.Application.Products.Commands;
+using WarehouseAPI.Application.CQRS.Products.Queries;
 
 namespace WarehouseAPI.Presentation.Controllers.v1
 {
@@ -21,6 +22,15 @@ namespace WarehouseAPI.Presentation.Controllers.v1
             var response = await _mediator.Send(command);
 
             return CreatedAtAction(nameof(Create), response);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var query = new GetAllProductsQuery();
+            var response = await _mediator.Send(query);
+
+            return Ok(response);
         }
     }
 }
