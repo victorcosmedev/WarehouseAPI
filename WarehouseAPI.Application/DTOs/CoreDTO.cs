029b97d563c93ba6d18efc1708f679a662492049
victorcosmedev <victorcosme.profissional@gmail.com>
2026-02-25 20:34:13 -0300

    feat: creating first DTOs

---
+++ WarehouseAPI.Application/DTOs/CoreDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseAPI.Application.DTOs;
+
+public class CoreDTO
+{
+    public int Id { get; set; }
+    public Guid ExternalId { get; set; }
+}
---
+++ WarehouseAPI.Application/DTOs/ProductDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using WarehouseAPI.Domain.Entities;
+
+namespace WarehouseAPI.Application.DTOs;
+
+public class ProductDTO : CoreDTO
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int Stock { get; set; }
+    public decimal Price { get; set; }
+    public bool Active { get; set; }
+
+
+}
